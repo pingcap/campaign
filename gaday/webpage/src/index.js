9a8b1cd21b8a14f330a1d78629692c294df211c5
gaohailang <ghlndsl@126.com>
2017-10-14 19:08:50 +0800

    chore(gaday): update index.js format

--- gaday/webpage/src/index.js
+++ gaday/webpage/src/index.js
@@ -9,74 +9,72 @@ import "utils/html5tooltips.js";
 const D = window.ContributorsData;
 
 let storage = window.localStorage;
-const IS_VISITED_KEY = 'visited_ga_day';
+const IS_VISITED_KEY = "visited_ga_day";
 let username = {};
 var timer;
 
 // render star wall
 const renderStarwall = () => {
-  console.log("setAttribute", document.getElementById("mask"))
-  document.getElementById("mask").setAttribute('style','display: block');
-  document.getElementById("starwall").setAttribute('style','display: block');
-}
+  console.log("setAttribute", document.getElementById("mask"));
+  document.getElementById("mask").setAttribute("style", "display: block");
+  document.getElementById("starwall").setAttribute("style", "display: block");
+};
 
 // render auth form
-const renderAuthForm = () => {}
+const renderAuthForm = () => {};
 
 if (!storage) {
-    console.log('not support localStorage')
+  console.log("not support localStorage");
 } else {
-    const visited = storage.getItem(IS_VISITED_KEY)
-    console.log(IS_VISITED_KEY, visited)
-    if (!visited) {
-      // render star wall
-      storage.setItem(IS_VISITED_KEY, username)
-      renderStarwall();
-      timer = setTimeout(renderAuthForm(), 3000)
-    } else {
-
-    }
+  const visited = storage.getItem(IS_VISITED_KEY);
+  console.log(IS_VISITED_KEY, visited);
+  if (!visited) {
+    // render star wall
+    storage.setItem(IS_VISITED_KEY, username);
+    renderStarwall();
+    timer = setTimeout(renderAuthForm(), 3000);
+  } else {
+  }
 }
 
 window.closeMask = () => {
-  document.getElementById("mask").setAttribute('style','display: none');
-  clearTimeout(timer)
-}
-
+  document.getElementById("mask").setAttribute("style", "display: none");
+  clearTimeout(timer);
+};
 
 // transform data to structure for rendering
 const isMobile = false; // isPc
 let ContriList = [];
 Object.keys(D).forEach(k => {
-    Object.keys(D[k].repos).forEach(r => {
-        ContriList.push({
-            name: k,
-            avatar: D[k].avatar,
-            repo: r,
-            level: D[k].repos[r]
-        });
+  Object.keys(D[k].repos).forEach(r => {
+    ContriList.push({
+      name: k,
+      avatar: D[k].avatar,
+      repo: r,
+      level: D[k].repos[r]
     });
+  });
 });
 console.log("ContriList length:", ContriList.length); // 400
 
 const rowPixelCount = isMobile ? 17 : 32;
 const spacePixelCount =
-    Math.ceil(ContriList.length / rowPixelCount) * rowPixelCount -
-    ContriList.length;
+  Math.ceil(ContriList.length / rowPixelCount) * rowPixelCount -
+  ContriList.length;
 console.log("spacePixelCount length:", spacePixelCount); // 400
 Array(spacePixelCount)
-    .fill({})
-    .forEach(i => {
-        ContriList.push({
-            name: `space-${i}`,
-            repo: null,
-            level: 0
-        });
+  .fill({})
+  .forEach(i => {
+    ContriList.push({
+      name: `space-${i}`,
+      repo: null,
+      level: 0
     });
+  });
 
 ContriList = shuffleArray(ContriList).map((i, idx) => {
-    i.idx = idx;
-    return i;
+  i.idx = idx;
+  return i;
 });
 // console.log("ContriList for render is ", ContriList);
 // random it contrilist
@@ -85,9 +83,9 @@ ContriList = shuffleArray(ContriList).map((i, idx) => {
 
 // render dbox
 const dboxHtml = window.tmpl(
-    `<div class="gaday-dbox" id="gaday-dbox"><% data.forEach((i)=>{ %><div class="mosaic-rect level-<%= i.level %>" id="pixel-id-<%= i.idx %>"></div> <% }) ;%></div>`
+  `<div class="gaday-dbox" id="gaday-dbox"><% data.forEach((i)=>{ %><div class="mosaic-rect level-<%= i.level %>" id="pixel-id-<%= i.idx %>"></div> <% }) ;%></div>`
 )({
-    data: ContriList
+  data: ContriList
 });
 // console.log(dboxHtml);
 document.getElementById("mosaic").innerHTML = dboxHtml;
@@ -98,13 +96,13 @@ document.getElementById("mosaic").innerHTML = dboxHtml;
 const visitorName = "queenypingcap";
 const visitorContriList = ContriList.filter(i => i.name === visitorName);
 if (visitorContriList.length) {
-    visitorContriList.forEach(i => {
-        document.getElementById(
-            `pixel-id-${i.idx}`
-        ).innerHTML = `<span>${i.repo}</span>`;
-    });
+  visitorContriList.forEach(i => {
+    document.getElementById(
+      `pixel-id-${i.idx}`
+    ).innerHTML = `<span>${i.repo}</span>`;
+  });
 } else {
-    // welcome bro - for newbie
+  // welcome bro - for newbie
 }
 
 // add hover handler for pixel
@@ -184,9 +182,9 @@ $dbox.addEventListener(
 );
 
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
