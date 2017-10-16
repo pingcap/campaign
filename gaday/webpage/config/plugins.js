58a9b5660cab99ce4315f4821a768c7fa0683210
gaohailang <ghlndsl@126.com>
2017-10-16 10:57:17 +0800

    feat(gaday): update gaday webpage build related task config

--- gaday/webpage/config/plugins.js
+++ gaday/webpage/config/plugins.js
@@ -6,7 +6,8 @@ import ExtractTextPlugin from "extract-text-webpack-plugin";
 import { NODE_ENV } from "./config";
 import { htmlIndex } from "./plugins/html";
 import { spritesheet } from "./plugins/assets";
-import { uglify, commons, manifest } from "./plugins/optimize";
+// import { uglify, commons, manifest } from "./plugins/optimize";
+import { uglify } from "./plugins/optimize";
 import {
   define,
   HMR,
@@ -29,9 +30,9 @@ export default [
   NODE_ENV === "production" ? extractCSS : 0,
   spritesheet,
   NODE_ENV === "production" ? uglify : 0,
-  commons,
-  manifest,
-  NODE_ENV === "production" ? banner : 0,
+  // commons,
+  // manifest,
+  // NODE_ENV === "production" ? banner : 0,
   NODE_ENV === "development" ? npmInstall : 0,
   NODE_ENV === "production" ? buildInfo : 0
 ].filter(Boolean);
--- gaday/webpage/config/plugins/optimize.js
+++ gaday/webpage/config/plugins/optimize.js
@@ -2,15 +2,16 @@ import webpack from "webpack";
 
 const uglify = new webpack.optimize.UglifyJsPlugin();
 
-const commons = new webpack.optimize.CommonsChunkPlugin({
-  name: "commons",
-  filename: "commons.js",
-  minChunks: module =>
-    module.context && module.context.indexOf("node_modules") !== -1
-});
+// const commons = new webpack.optimize.CommonsChunkPlugin({
+//   name: "commons",
+//   filename: "commons.js",
+//   minChunks: module =>
+//     module.context && module.context.indexOf("node_modules") !== -1
+// });
+//
+// const manifest = new webpack.optimize.CommonsChunkPlugin({
+//   name: "manifest"
+// });
 
-const manifest = new webpack.optimize.CommonsChunkPlugin({
-  name: "manifest"
-});
-
-export { uglify, commons, manifest };
+// export { uglify, commons, manifest };
+export { uglify };
